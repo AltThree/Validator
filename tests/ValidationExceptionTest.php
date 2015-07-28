01fc9ab865df38254d03c749f6a266529a2df392
Graham Campbell <graham@alt-three.com>
2015-07-29 00:55:40 +0100

    Added a couple of basic tests

---
+++ tests/ValidationExceptionTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of Alt Three Validator.
+ *
+ * (c) Alt Three Services Limited
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AltThree\Tests\Validator;
+
+use AltThree\Validator\ValidationException;
+use Illuminate\Support\MessageBag;
+use PHPUnit_Framework_TestCase;
+
+/**
+ * This is the validation exception test class.
+ *
+ * @author Graham Campbell <graham@alt-three.com>
+ */
+class ValidationExceptionTest extends PHPUnit_Framework_TestCase
+{
+    public function testExceptionMessageIsCorrect()
+    {
+        $e = new ValidationException(new MessageBag([]));
+
+        $this->assertSame('Validation has failed.', $e->getMessage());
+    }
+
+    public function testExceptionMessageBagWorks()
+    {
+        $e = new ValidationException(new MessageBag(['123']));
+
+        $this->assertSame(['123'], $e->getMessageBag()->all());
+    }
+}
